8ac715e3605f04fb244f57ce4b22d3e331b3c936
thaisNY <71556315+thaisNY@users.noreply.github.com>
2022-02-05 20:12:38 -0300

    Create var_let_const.js

---
+++ seccao4/var_let_const.js
@@ -0,0 +1,11 @@
+var a = 2
+let b = 3
+//trocando os valores das variaveis
+
+var a = 20
+//let = 40, exeberia um erro,não acontece o mesmo com var
+b = 40
+const c = 100
+console.log(a,b,c)
+//tipos do java string,number e boolean
+ 
