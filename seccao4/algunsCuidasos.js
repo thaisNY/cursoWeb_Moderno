a38ef6a29201e994562b2038bfa88c5d43188a90
thaisNY <71556315+thaisNY@users.noreply.github.com>
2022-03-12 08:57:33 -0300

    Create algunsCuidasos.js

---
+++ seccao4/algunsCuidasos.js
@@ -0,0 +1,6 @@
+console.log(7/0) //infinity
+console.log("10"/2) //5
+console.log("show"*2) //Nan
+console.log(0.1 + 0.7)//0.7999999 imprecisão com ponto flutuante
+//console.log(10.toString()) erro
+console.log((10.345).toFixed(2))//10.35
